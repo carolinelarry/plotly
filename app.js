0a5ead753eddc6ab3851b07942270a4e1c6046f4
Caroline Larry <carolinelarry@Carolines-MBP.attlocal.net>
2021-06-25 18:23:22 -0500

    Adding new changes

--- app.js
+++ app.js
@@ -1,62 +1,115 @@
-//<select id="selDataset" onchange="optionChanged(this.value)"></select>
+//Initialzing function
+function init(){
+d3.json("samples.json").then(function(data){
+    var dropdownMenu = d3.select("#selDataset");
+
+    var ids = data.names;
+        
+    ids.forEach((sample) => {
+        dropdownMenu
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
+});
+
+    data=[{
+        x: [0,10,20,30],
+        y: [10,10,10,10],
+        type: "bar",
+        orientation: 'h'
+    }];
+
+    Plotly.newPlot("bar", data);
+};
 
 
-//d3.select("#selDataset").on("change", updatePlotly);
+//When you change dropdown option, calls buildPlot function
+d3.selectAll("#selDataset").on("change", buildPlot);
 
-// d3.json(samples.json).then(function(data) {
-//     console.log(data);
-// });
-
-// const dataPromise = d3.json(samples.json);
-// console.log("Data Promise: ", dataPromise);
-
-
-
-//Since we have json within a json, this will get you sample_values array; before it was just json
-function buildPLot(){
+//Overall function
+function buildPlot(){
     d3.json("samples.json").then(function(data) {
-        var name = data.samples[0];
-        var samples_values = name.sample_values;
-        //console.log(samples_values);
-        var labels = name.otu_ids;
-        //console.log(data);
-        var ids = data.names;
-        console.log(ids);
 
-        //Slice for plotting
-        var slicedData1 = samples_values.slice(0,10);
-        //console.log(slicedData1);
-        var slicedData2 = String(labels.slice(0,10));
-        //console.log(slicedData2);
+    //Sets up dropdown menu with ids
+    var dropdownMenu = d3.select("#selDataset");
+    var ids = data.names;  
+    ids.forEach((sample) => {
+        dropdownMenu
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
+
+    //Finding name of selected id
+    var selectedID = dropdownMenu.property("value");
+    //console.log(selectedID);
+
+        //var new1 = data.samples.id;
+        //console.log(new1);
+
+    //Creating function that finds id that is equal to selectedID 
+    //THIS WORKS!!!!
+    function filterID(item){
+        return item.id == selectedID;
+    };   
+
+    //This is the sample entry that has the same ID as selectedID
+    //filtering samples array by filterID function
+    var sampleItem = data.samples.filter(filterID);
+    console.log(sampleItem);
+
+      
+
+        //var name = data.samples[0];
+        //var samples_values = name.sample_values;
+        //console.log(samples_values);
+        //var otuIds = name.otu_ids;
+        //console.log(data);
+        // var ids = data.names;
+        // console.log(ids);
+
+        
+    //Selecting top 10 sample_values for our sample_item
+    var slicedData1 = sampleItem[0].sample_values.slice(0,10);
+    console.log(slicedData1);
+    var slicedData2 = sampleItem[0].otu_ids.slice(0,10);
+    console.log(slicedData2);
         
         //Will return key value pairs for table
         var keys = Object.entries(data.metadata);
         //console.log(keys);
+    
+    // var x = [];
+    // var y = [];
 
-        var data = [{
-            x: slicedData1,
-            y: slicedData2,
-            type:"bar",
+    //ADD THIS BACK AT END WHEN READY FOR GRAPH
+    // var data = [{
+    //      x = slicedData1,
+    //      y = slicedData2,
+    //      //type:"bar",
         
-        }];
+    //      }];
 
-        //This works and shows up in dropdown but shows up as undefined
-        var dropdownMenu = d3.select("#selDataset")
-         ids.forEach(item => (dropdownMenu.html(function(item) {
-             return `<option value="${item}">${item}</option>`})));
+    var x = slicedData1;
+    var y = slicedData2;
 
         
 
+        //Finding value of our id
+        // var idNum = dropdownMenu.property("value");
+        // console.log(`idNum is ${idNum}`);
         
-
-        Plotly.newPlot("bar", data);
+    //FIX LATER: X IS NOT DEFINED
+    Plotly.restyle("bar", "x", [x]);
+    Plotly.restyle("bar", "y", [y]);
 
     })
 
     
-}
+};
 
-buildPLot();
+init();
 
 
 
