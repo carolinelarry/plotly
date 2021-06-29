486cc84431cd46cdb20c92bc9549fb343f407b4f
Caroline Larry <carolinelarry@Carolines-MBP.attlocal.net>
2021-06-29 10:21:27 -0500

    Adding final file changes

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ function init(){
 d3.json("samples.json").then(function(data){
     var dropdownMenu = d3.select("#selDataset");
 
+    //Getting ids to populate dropdown menu
     var ids = data.names;
         
+    //Populating dropdown menu
     ids.forEach((sample) => {
         dropdownMenu
         .append("option")
@@ -23,21 +25,25 @@ d3.json("samples.json").then(function(data){
 
 
     //Creating bar graph
+
     //Selecting top 10 sample_values for our sample_item
     var slicedData1940 = initPlotData[0].sample_values.slice(0,10);
     //console.log(slicedData1940);
+
     //Selecting top 10 otu_ids for our sample_item
     var slicedData2940 = initPlotData[0].otu_ids.slice(0,10);
     //console.log(slicedData2940);
+
     //Selecting top 10 otu_labels for our sample_item
     var hoverTextData940 = initPlotData[0].otu_labels.slice(0,10);
     //console.log(hoverTextData940);
 
-    //var xBar940 = slicedData1940;
-    //It's using the index instead of the value
+    
+    //Creating our y axis labels
     var yBar940 = slicedData2940.map(x => `ID ${x}`);
     //console.log(yBar940);
 
+    //Creating data for bar graph for id 940
     var dataBar = [{
         x: slicedData1940.reverse(),
         y: yBar940.reverse(),
@@ -54,19 +60,24 @@ d3.json("samples.json").then(function(data){
     Plotly.newPlot("bar", dataBar, layout, config);
 
     //Creating bubble graph
+
     //Selecting top 10 sample_values for our sample_item
     var sampleValues940 = initPlotData[0].sample_values;
     //console.log(sampleValues940);
+
     //Selecting top 10 otu_ids for our sample_item
     var OtuID940 = initPlotData[0].otu_ids;
     //console.log(OtuID940);
+
     //Selecting top 10 otu_labels for our sample_item
     var OtuLabels940 = initPlotData[0].otu_labels;
     //console.log(OtuLabels940);
 
+    //Creating variables for x and y values for bubble graph for id 940
     var xBubble940 = OtuID940;
     var yBubble940 = sampleValues940;
 
+    //Data for id 940, bubble chart
     var dataBubble940 = [{
             x: xBubble940,
             y: yBubble940.reverse(),
@@ -80,59 +91,31 @@ d3.json("samples.json").then(function(data){
         title: "Bacteria Cultures Per Sample"
     }
 
+    //Plotting bubble chart for id 940
     Plotly.newPlot("bubble", dataBubble940, layout, config);
 
 
-    //function to get entry for metadata with id that matches selectedID
-    // function filterKeys(item){
-    //     return item.id == 940;
-    // };
+    //Demographic info box section
 
-    //DEMOGRAPHIC INFO SECTION
-    //Calling that function on our data entry
+    //Filtering our metadata to find info in id 940
     var demBox940 = data.metadata.filter(id940);
     //console.log(`demInfo is ${demBox940}`);
 
     var demInfoJson = demBox940[0];
-    console.log(demInfoJson);
+    //console.log(demInfoJson);
 
-    //var demInfoKeys = Object.keys(demInfoJson);
-    //console.log(demInfoKeys);
-
+    //Selecting id where we are going to place dem. info keys/ values
     var demInfoPanel = d3.select("#sample-metadata");
 
+    //Clearing out any info that's in the dem. info box
     demInfoPanel.html('');
+
+    //Placing key/ value pairs for id 940
     Object.entries(demInfoJson).map(([key, value]) => {
         demInfoPanel.append("p").html(`<b>${key}:</b> ${value}`)
     });
 
-
-
-
 });
-
-    //INIT FUNCTION
-    // dataBar =[{
-    //     x: [10,20,30,40],
-    //     y: ["ID 1", "ID 2", "ID 3", "ID 4"],
-    //     text: ['Text 1', 'Text 2', 'Text 3', 'Text 4'],
-    //     type: "bar",
-    //     orientation: 'h'
-    // }];
-
-    // Plotly.newPlot("bar", dataBar);
-
-    // dataHover = [{
-    //     x: [10,20,30,40],
-    //     y: [10,12,13,14],
-    //     mode: 'markers',
-    //     marker: {size:[40,60,80,100], color: ['green', 'green', 'green', 'green']}
-
-    // }];
-
-    // Plotly.newPlot("bubble", dataHover);
-    //END OF INIT FUNCTION
-
 
 
 };
@@ -141,7 +124,7 @@ d3.json("samples.json").then(function(data){
 //When you change dropdown option, calls buildPlot function
 d3.selectAll("#selDataset").on("change", buildPlot);
 
-//Overall function
+//Function when dropdown menu is changed
 function buildPlot(){
     d3.json("samples.json").then(function(data) {
 
@@ -156,40 +139,27 @@ function buildPlot(){
     });
 
 
-    //Finding name of selected id
+    //Finding selected id
     var selectedID = dropdownMenu.property("value");
     //console.log(selectedID);
-
-        //var new1 = data.samples.id;
-        //console.log(new1);
 
     //Creating function that finds id that is equal to selectedID 
     function filterID(item){
         return item.id == selectedID;
     };   
 
-    //This is the sample entry that has the same ID as selectedID
-    //filtering samples array by filterID function
+    //Filtering samples array by filterID function (by id that was selected)
     var sampleItem = data.samples.filter(filterID);
     //console.log(sampleItem);
-
-      
-
-        //var name = data.samples[0];
-        //var samples_values = name.sample_values;
-        //console.log(samples_values);
-        //var otuIds = name.otu_ids;
-        //console.log(data);
-        // var ids = data.names;
-        // console.log(ids);
-
         
     //Selecting top 10 sample_values for our sample_item
     var slicedData1 = sampleItem[0].sample_values.slice(0,10);
     //console.log(slicedData1);
+
     //Selecting top 10 otu_ids for our sample_item
     var slicedData2 = sampleItem[0].otu_ids.slice(0,10);
     //console.log(slicedData2);
+
     //Selecting top 10 otu_labels for our sample_item
     var hoverTextData = sampleItem[0].otu_labels.slice(0,10);
     //console.log(hoverTextData);
@@ -203,17 +173,16 @@ function buildPlot(){
         return item.id == selectedID;
     };
 
-    //DEMOGRAPHIC INFO SECTION
-    //Calling that function on our data entry
+    //Demographic Info box section
+
+    //Filtering our metadata by the selected id
     var demInfo = data.metadata.filter(filterKeys);
     //console.log(demInfo);
 
     var demInfoJson = demInfo[0];
     //console.log(demInfoJson);
 
-    //var demInfoKeys = Object.keys(demInfoJson);
-    //console.log(demInfoKeys);
-
+    //Selecting id that we want to place our data into in html file
     var demInfoPanel = d3.select("#sample-metadata");
 
     demInfoPanel.html('');
@@ -221,44 +190,35 @@ function buildPlot(){
         demInfoPanel.append("p").html(`<b>${key}:</b> ${value}`)
     });
 
-    //console.log(demInfoEntries);
+    //Creating graphs
 
-    //Placing key/value pairs in html file
-    //var demInfoID = d3.select("#sample-metadata")//.text(demInfoEntries);
-
-    // demInfoEntries.forEach((item) => {
-    //     demInfoID.text(item);
-    // }
-
-    //)
-
-
-
+    //Creating x and y variables for bar graph
     var xBar = slicedData1;
-    //It's using the index instead of the value
     var yBar = slicedData2.toString();
     //console.log(y);
 
 
     //Selecting top 10 sample_values for our sample_item
     var sampleValues = sampleItem[0].sample_values;
-    //console.log("Update")
     //console.log(sampleValues);
+
     //Selecting top 10 otu_ids for our sample_item
     var OtuID = sampleItem[0].otu_ids;
     //console.log(OtuID);
+
     //Selecting top 10 otu_labels for our sample_item
     var OtuLabels = sampleItem[0].otu_labels;
     //console.log(OtuLabels);
 
+    //Creating x and y variables for bubble chart
     var xBubble = OtuID;
     var yBubble = sampleValues;
 
-
-    //It's using the index instead of the value
+    //Creating y axis labels 
     var yBar = slicedData2.map(x => `ID ${x}`);
     //console.log(yBar940);
 
+    //Creating data for bar chart
     var dataBar = [{
         x: slicedData1.reverse(),
         y: yBar.reverse(),
@@ -271,16 +231,10 @@ function buildPlot(){
         title: "Top 10 Bacteria Cultures Found"
     }
 
+    //Plotting bar chart for selected id
     var config = {responsive: true};
     Plotly.newPlot("bar", dataBar, layout1, config);
 
-
-
-        
-
-        //Finding value of our id
-        // var idNum = dropdownMenu.property("value");
-        // console.log(`idNum is ${idNum}`);
         
     //FIX LATER: X IS NOT DEFINED
     // Plotly.restyle("bar", "x", [xBar]);
@@ -291,6 +245,7 @@ function buildPlot(){
     // Plotly.restyle("bubble", "y", yBubble);
     // Plotly.restyle("bubble", "size", sampleValues);
 
+    //Creatind data array for bubble chart for selected id
     var bubbleData = [{
         x: xBubble,
         y: yBubble.reverse(),
@@ -303,7 +258,7 @@ function buildPlot(){
         title: "Bacteria Cultures Per Sample"
     }
 
-
+    //Plotting bubble chart
     Plotly.newPlot("bubble", bubbleData, layout2, config);
 
 
@@ -315,34 +270,3 @@ function buildPlot(){
 
 init();
 
-
-
-
-
-
-//Original code
-// function updatePlotly(){
-//     var dropdownMenu = d3.select("#selDataset");
-//     var idValue = dropdownMenu.property("value");
-
-//     var x = [];
-//     var y= [];
-
-//     //create for loop for ids?
-
-//     if(idValue === 'id940'){
-//         x = [samples.samples.sample_values];
-//         console.log(x);
-//         y = [samples.samples.otu_ids];
-//         console.log(y);
-//     };
-
-//     var data = [{
-//         x: x,
-//         y: y,
-//     }];
-
-//     Plotly.newPlot("bar", data);
-    
-
-// };
